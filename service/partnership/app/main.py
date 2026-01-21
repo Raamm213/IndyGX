a142ed1f270ae05b0de964cd0a9244860cbdebb2
Rethees Raamm R S <retheesrs@gmail.com>
2026-01-21 12:22:42 +0530

    minor changes in the partnership microservice

--- service/partnership/app/main.py
+++ service/partnership/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .api.company_partnership_routes import router
 
-app = FastAPI(title="Company Indygx-Assessment Service")
+app = FastAPI(title="Company Partnership Service")
 
 app.include_router(router)
  
